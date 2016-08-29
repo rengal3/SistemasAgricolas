e3feeb4c0b3b14343248ee2578619a74ec14adfe
Renato <Renato@192.168.0.12>
2016-08-29 11:29:36 -0500

    -entidades

---
+++ Asistencia-cliente/src/asistencia/delegate/GestionAsistenciaDelegate.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package asistencia.delegate;
+
+import com.asistencia.TO.AdmisionTipoTO;
+import com.asistencia.facade.AsistenciaFacadeRemote;
+import java.util.Properties;
+import javax.naming.InitialContext;
+
+/**
+ *
+ * @author Renato
+ */
+public class GestionAsistenciaDelegate {
+     private static GestionAsistenciaDelegate gestionDelegate;
+    private AsistenciaFacadeRemote gestionFacade;
+    
+    private GestionAsistenciaDelegate() throws Exception{
+        try{
+         Properties props = new Properties();
+            /*props.setProperty("java.naming.factory.initial", "com.sun.enterprise.naming.SerialInitContextFactory");
+            props.setProperty("java.naming.factory.url.pkgs", "com.sun.enterprise.naming");
+            props.setProperty("java.naming.factory.state", "com.sun.corba.ee.impl.presentation.rmi.JNDIStateFactoryImpl");
+            */props.setProperty("org.omg.CORBA.ORBInitialHost", "127.0.0.1");
+            props.setProperty("org.omg.CORBA.ORBInitialPort", "3700");
+         
+          InitialContext ctx = new InitialContext(props);  
+            
+           //InitialContext ctx = new InitialContext();
+          this.gestionFacade = ((AsistenciaFacadeRemote)
+                  ctx.lookup("com.asistencia.facade.AsistenciaFacadeRemote"));
+
+        }
+        catch (Exception e) {
+            throw e;
+        }
+    }
+    
+    public static GestionAsistenciaDelegate getInstance() throws Exception {
+    if (gestionDelegate == null) {
+      gestionDelegate = new GestionAsistenciaDelegate();
+    }
+    return gestionDelegate;
+  }
+
+    public boolean insertaAdmisionTipo(AdmisionTipoTO admision) throws Exception {
+        return gestionFacade.insertarAdmisionTipo(admision);
+    }
+
+   
+}
